5ebf116ec13577a2cddbfff8eed3031336fdd31b
Kat Marchán <kzm@zkat.tech>
2018-11-05 21:18:21 -0800

    fix(cache): fix --cache option to let it be provided in cli

--- bin/tink.js
+++ bin/tink.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-require('../lib/node/index.js')
-
 const CMDS = new Map([
   ['sh', require('../lib/commands/shell.js')],
   ['shell', require('../lib/commands/shell.js')],
@@ -19,17 +17,7 @@ function main (argv) {
   const log = require('npmlog')
   log.heading = 'tink'
   const npmConfig = require('../lib/config.js')
-  if (needsYargs(argv)) {
-    return runCommandWithYargs(argv, log, npmConfig)
-  } else {
-    return noYargsShortcut(argv, log, npmConfig)
-  }
-}
-
-function needsYargs (argv) {
-  return argv.length > 3 && (
-    argv[3] !== '--' && argv[3].match(/^--?[a-z0-9]+/i)
-  )
+  return runCommandWithYargs(argv, log, npmConfig)
 }
 
 function runCommandWithYargs (argv, log, npmConfig) {
@@ -44,18 +32,10 @@ function runCommandWithYargs (argv, log, npmConfig) {
   for (const mod of CMDS.values()) {
     config = config.command(mod)
   }
+  require('../lib/node/index.js')
   const yargv = npmConfig(config.argv).concat({ log })
-  log.level = yargv.loglevel || 'notice'
-}
-
-function noYargsShortcut (argv, log, npmConfig) {
-  // This is an optimization because Yargs can be expensive to load.
-  const opts = npmConfig({ log, _: argv.slice(2) })
-  log.level = opts.loglevel
-  const cmd = CMDS.get(argv[2])
-  if (!cmd) {
-    return runCommandWithYargs(argv, log, npmConfig)
-  } else {
-    return cmd.handler(opts)
+  process.tink = {
+    cache: yargv.cache
   }
+  log.level = yargv.loglevel || 'notice'
 }
