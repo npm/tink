85b440fdbc15a03010387eb5d7012c0a77027962
Alejandro López <a.lopez.sanchez@outlook.es>
2018-11-13 13:25:34 +0100

    feat(profile): add `create-token` subcommand

--- lib/commands/profile.jsx
+++ lib/commands/profile.jsx
@@ -10,37 +10,41 @@ const Profile = module.exports = {
       .demandCommand(1, 'Profile subcommand is required')
       .recommendCommands()
       .command({
+        command: 'create-token',
+        describe: 'Create a new authentication token, possibly with restrictions.',
+        builder: y => y.help('help', 'h').options(Object.assign({}, ProfileSubcommandsOptions , {
+          'read-only': {
+            alias: 'ro',
+            describe: 'Readonly',
+            type: 'boolean',
+            default: false
+          },
+          cidr_whitelist: {
+            alias: 'cidr',
+            describe: 'CIDR ranges to limit use of this token to.',
+            type: 'array',
+            default: []
+          }
+        })),
+        handler: argv => createToken(argv)
+      })
+      .command({
         command: 'list-tokens',
         aliases: ['lt'],
         describe: 'Fetch a list of all of the authentication tokens the authenticated user has.',
-        builder: y => y.help('help', 'h').options({
-          token: {
-            alias: 't',
-            describe: 'Used for Bearer auth',
-            type: 'string'
-          },
-          username: {
-            alias: 'u',
-            describe: 'Used for Basic auth',
-            type: 'string'
-          },
-          password: {
-            alias: 'p',
-            describe: 'Used for Basic auth',
-            type: 'string'
-          }
-        }),
+        builder: y => y.help('help', 'h').options(ProfileSubcommandsOptions),
         handler: argv => listTokens(getOptions(argv))
       })
   },
   options: Object.assign({}, require('../common-opts'), {})
-};
+}
 
 const libnpm = require('libnpm')
 const figgyPudding = require('figgy-pudding')
 const { h, renderToString } = require('ink')
 const Table = require('ink-table').default
 const log = require('npmlog')
+const readPassword = require("../utils/read-password");
 const npmConfig = require('../config.js')
 
 const ProfileConfig = figgyPudding({
@@ -53,10 +57,67 @@ const ProfileConfig = figgyPudding({
   password: {}
 })
 
+const ProfileSubcommandsOptions = {
+  token: {
+    alias: 't',
+    describe: 'Used for Bearer auth',
+    type: 'string'
+  },
+  username: {
+    alias: 'u',
+    describe: 'Used for Basic auth',
+    type: 'string'
+  },
+  password: {
+    alias: 'p',
+    describe: 'Used for Basic auth',
+    type: 'string'
+  }
+}
+
+const tableHeaders = ['token', 'key', 'cidr_whitelist', 'readonly', 'created', 'updated']
+
 const getOptions = argv => ProfileConfig(npmConfig().concat(argv).concat({ log }))
 
 // TODO: Move to `../utils`?
-const logError = (err) => console.error(`Error code: ${err.code} => ${err.message}`);
+const logError = (err) => console.error(`Error code: ${err.code} => ${err.message}`)
+
+const mapTokenToTable = (token, options = { trimToken: true }) => {
+  token.key = token.key.substring(0, 6)
+  token.token = options.trimToken ? token.token.substring(0, 6) + '...' : token.token
+  token.readonly = token.readonly ? 'yes' : 'no'
+  return token
+}
+
+// TODO: OTP code
+async function createToken(argv) {
+  const opts = getOptions(argv)
+
+  try {
+    const password = await readPassword()
+    const newToken = await libnpm.profile.createToken(
+      password,
+      argv['read-only'],
+      argv.cidr_whitelist,
+      opts
+    )
+
+    if (opts.json) {
+      console.log(JSON.stringify(newToken, null, 2))
+    } else if (opts.parseable) {
+      console.log(tableHeaders.join('\t'))
+      let values = tableHeaders
+        .map(header => newToken[header])
+        .reduce((previous, current) => `${previous}\t${current}`)
+      console.log(values)
+    } else if (!opts.silent && opts.loglevel !== 'silent') {
+      const data = [mapTokenToTable(newToken, { trimToken: false })]
+      console.log(renderToString(<Table data={data}/>))
+    }
+  } catch (e) {
+    logError(e)
+  }
+}
 
 async function listTokens (opts) {
   try {
@@ -65,21 +126,15 @@ async function listTokens (opts) {
     if (opts.json) {
       console.log(JSON.stringify(tokens, null, 2))
     } else if (opts.parseable) {
-      const headers = ['token', 'key', 'cidr_whitelist', 'readonly', 'created', 'updated']
-      console.log(headers.join('\t'))
+      console.log(tableHeaders.join('\t'))
       tokens.forEach(token => {
-        let values = headers
+        let values = tableHeaders
           .map(header => token[header])
           .reduce((previous, current) => `${previous}\t${current}`)
         console.log(values)
       })
     } else if (!opts.silent && opts.loglevel !== 'silent') {
-      const data = tokens.map(token => {
-        token.key = token.key.substring(0, 6);
-        token.token = token.token.substring(0, 6) + '...'
-        token.readonly = token.readonly ? 'yes' : 'no'
-        return token
-      });
+      const data = tokens.map(token => mapTokenToTable(token))
       console.log(renderToString(<Table data={data}/>))
     }
   } catch (e) {
