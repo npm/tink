b7f7fd78acce09a7c058e5eb443fecdc00d1733f
Kat Marchán <kzm@zkat.tech>
2018-11-20 11:55:12 -0800

    fix(test): refactor fs tests and get them passing again

--- test/node/fs.js
+++ test/node/fs.js
@@ -2,78 +2,78 @@
 
 const fs = require('fs')
 const path = require('path')
-const tap = require('tap')
+const { test } = require('tap')
 const { overrideNode } = require('../../lib/node/fs')
 
 overrideNode()
 
 // lstatSync
 
-tap.test('lstatSync', function (test) {
+test('lstatSync', t => {
   const stat = fs.lstatSync(path.resolve('test'))
-  test.ok(stat, 'lstatSync should return valid result')
-  test.true(stat.isDirectory(), 'lstatSync should set isDirectory true')
-  test.end()
+  t.ok(stat, 'lstatSync should return valid result')
+  t.true(stat.isDirectory(), 'lstatSync should set isDirectory true')
+  t.end()
 })
 
-tap.test('lstatSync', function (test) {
+test('lstatSync', t => {
   try {
     fs.lstatSync(path.resolve(`foo-${Date.now()}-test`))
-    test.fail('expect lstatSync to throw')
+    t.fail('expect lstatSync to throw')
   } catch (err) {}
-  test.end()
+  t.end()
 })
 
 // lstat
 
-tap.test('lstat', function (test) {
+test('lstat', t => {
   fs.lstat(path.resolve('test'), (err, stat) => {
-    test.notOk(err, `lstat should not fail with error`)
-    test.ok(stat, 'lstat should return valid result')
-    test.true(stat.isDirectory(), 'lstat should set isDirectory true')
-    test.end()
+    t.notOk(err, `lstat should not fail with error`)
+    t.ok(stat, 'lstat should return valid result')
+    t.true(stat.isDirectory(), 'lstat should set isDirectory true')
+    t.end()
   })
 })
 
-tap.test('lstat', function (test) {
+test('lstat', t => {
   fs.lstat(path.resolve(`foo-${Date.now()}-test`), (err, stat) => {
-    test.ok(err, 'expect lstat to fail')
-    test.end()
+    t.ok(err, 'expect lstat to fail')
+    t.end()
   })
 })
 
 // statSync
 
-tap.test('statSync', function (test) {
+test('statSync', t => {
   const stat = fs.statSync(path.resolve('test'))
-  tap.ok(stat, 'statSync should return valid result')
-  tap.true(stat.isDirectory(), 'statSync should set isDirectory true')
-  test.end()
+  t.ok(stat, 'statSync should return valid result')
+  t.true(stat.isDirectory(), 'statSync should set isDirectory true')
+  t.end()
 })
 
-tap.test('statSync', function (test) {
+test('statSync', t => {
   try {
     fs.statSync(path.resolve(`foo-${Date.now()}-test`))
-    test.fail('expect statSync to throw')
+    t.fail('expect statSync to throw')
   } catch (err) {}
-  test.end()
+  t.end()
 })
 
 // stat
 
-tap.test('stat', function (test) {
+test('stat', t => {
   fs.stat(path.resolve('test'), (err, stat) => {
-    test.notOk(err, `stat should not fail with error`)
-    test.ok(stat, 'stat should return valid result')
-    test.true(stat.isDirectory(), 'stat should set isDirectory true')
-    test.end()
+    t.notOk(err, `stat should not fail with error`)
+    t.ok(stat, 'stat should return valid result')
+    t.true(stat.isDirectory(), 'stat should set isDirectory true')
+    t.end()
   })
 })
 
-tap.test('stat', function (test) {
+test('stat', t => {
   fs.stat(path.resolve(`foo-${Date.now()}-test`), (err, stat) => {
-    test.ok(err, 'expect stat to fail')
-    test.end()
+    t.ok(err, 'expect stat to fail')
+    t.end()
   })
 })
 
@@ -81,39 +81,39 @@ tap.test('stat', function (test) {
 
 // realpathSync
 
-tap.test('realpathSync', function (test) {
+test('realpathSync', t => {
   const rp = fs.realpathSync('.')
-  test.equal(rp, process.cwd(), 'realpathSync should return cwd')
+  t.equal(rp, process.cwd(), 'realpathSync should return cwd')
   // test exception
   try {
     fs.realpathSync(`/non-existing-dir-${Date.now()}`)
-    test.fail(`realpathSync should throw`)
+    t.fail(`realpathSync should throw`)
   } catch (e) {}
-  test.end()
+  t.end()
 })
 
-tap.test('realpathSync', function (test) {
+test('realpathSync', t => {
   try {
     fs.realpathSync(`/non-existing-dir-${Date.now()}`)
-    test.fail(`realpathSync should throw`)
+    t.fail(`realpathSync should throw`)
   } catch (e) {}
-  test.end()
+  t.end()
 })
 
 // realpath
 
-tap.test('realpath', function (test) {
+test('realpath', t => {
   fs.realpath('.', (err, rp) => {
-    test.notOk(err, 'realpath should not fail')
-    test.equal(rp, process.cwd(), 'realpath should return cwd')
-    test.end()
+    t.notOk(err, 'realpath should not fail')
+    t.equal(rp, process.cwd(), 'realpath should return cwd')
+    t.end()
   })
 })
 
-tap.test('realpath', function (test) {
+test('realpath', t => {
   fs.realpath(`/non-existing-dir-${Date.now()}`, (err, rp) => {
-    test.ok(err, 'realpath should fail with error')
-    test.end()
+    t.ok(err, 'realpath should fail with error')
+    t.end()
   })
 })
 
@@ -121,198 +121,198 @@ tap.test('realpath', function (test) {
 
 // existsSync
 
-tap.test('existsSync', function (test) {
+test('existsSync', t => {
   const flag = fs.existsSync(path.resolve('test'))
-  test.equal(flag, true, 'exists should return true')
-  test.end()
+  t.equal(flag, true, 'exists should return true')
+  t.end()
 })
 
-tap.test('existsSync', function (test) {
+test('existsSync', t => {
   const flag = fs.existsSync(`/non-existing-dir-${Date.now()}`)
-  test.equal(flag, false, 'exists should return false')
-  test.end()
+  t.equal(flag, false, 'exists should return false')
+  t.end()
 })
 
 // access
 
-tap.test('access', function (test) {
+test('access', t => {
   fs.access(path.resolve('test'), (err) => {
-    test.notOk(err, 'access should return OK')
-    test.end()
+    t.notOk(err, 'access should return OK')
+    t.end()
   })
 })
 
-tap.test('access', function (test) {
+test('access', t => {
   fs.access(path.resolve('test'), fs.constants.F_OK, (err, result) => {
-    test.notOk(err, 'access should return OK with F_OK mode')
-    test.end()
+    t.notOk(err, 'access should return OK with F_OK mode')
+    t.end()
   })
 })
 
-tap.test('access', function (test) {
+test('access', t => {
   fs.access(path.resolve('package.json'), fs.constants.R_OK, (err) => {
-    test.notOk(err, 'access should return OK with R_OK mode')
-    test.end()
+    t.notOk(err, 'access should return OK with R_OK mode')
+    t.end()
   })
 })
 
-tap.test('access', function (test) {
+test('access', t => {
   fs.access(path.resolve(`/non-existing-dir-${Date.now()}`), fs.constants.R_OK, (err) => {
-    test.equal(err.code, 'ENOENT', 'access fail with ENOENT')
-    test.end()
+    t.equal(err.code, 'ENOENT', 'access fail with ENOENT')
+    t.end()
   })
 })
 
-tap.test('access', function (test) {
+test('access', t => {
   fs.access(path.resolve(`/etc/passwd`), fs.constants.W_OK, (err) => {
-    test.equal(err.code, 'EACCES', 'access fail with EACCES for W_OK on /etc/passwd')
-    test.end()
+    t.ok(err.code === 'EACCES' || err.code === 'EPERM', 'access fail with right code for W_OK on /etc/passwd')
+    t.end()
   })
 })
 
 // accessSync
 
-tap.test('accessSync', function (test) {
+test('accessSync', t => {
   try {
     fs.accessSync(path.resolve('test'))
-    test.end()
+    t.end()
   } catch (err) {
-    test.fail('accessSync should return OK')
+    t.fail('accessSync should return OK')
   }
 })
 
-tap.test('accessSync', function (test) {
+test('accessSync', t => {
   try {
     fs.accessSync(path.resolve('test'), fs.constants.F_OK)
-    test.end()
+    t.end()
   } catch (err) {
-    test.fail('accessSync should return OK with F_OK mode')
+    t.fail('accessSync should return OK with F_OK mode')
   }
 })
 
-tap.test('accessSync', function (test) {
+test('accessSync', t => {
   try {
     fs.accessSync(path.resolve('package.json'), fs.constants.R_OK)
-    test.end()
+    t.end()
   } catch (err) {
-    test.fail('accessSync should return OK with R_OK mode')
+    t.fail('accessSync should return OK with R_OK mode')
   }
 })
 
-tap.test('accessSync', function (test) {
+test('accessSync', t => {
   try {
     fs.accessSync(path.resolve(`/non-existing-dir-${Date.now()}`), fs.constants.R_OK)
-    test.fail('accessSync should fail with ENOENT')
+    t.fail('accessSync should fail with ENOENT')
   } catch (err) {
-    test.end()
+    t.end()
   }
 })
 
-tap.test('accessSync', function (test) {
+test('accessSync', t => {
   try {
     fs.accessSync(path.resolve(`/etc/passwd`), fs.constants.W_OK)
-    test.fail('accessSync should fail with EACCES for W_OK on /etc/passwd')
+    t.fail('accessSync should fail with EACCES for W_OK on /etc/passwd')
   } catch (err) {
-    test.end()
+    t.end()
   }
 })
 
 // readFile
 
-tap.test('readFile', function (test) {
+test('readFile', t => {
   fs.readFile(path.resolve('package.json'), (err, data) => {
-    test.notOk(err, 'readFile should be OK')
+    t.notOk(err, 'readFile should be OK')
     try {
-      test.equal(Buffer.isBuffer(data), true, 'readFile should load package.json as Buffer')
+      t.equal(Buffer.isBuffer(data), true, 'readFile should load package.json as Buffer')
       JSON.parse(data)
-      test.end()
+      t.end()
     } catch (err) {
-      test.fail('readFile should load package.json')
+      t.fail('readFile should load package.json')
     }
   })
 })
 
-tap.test('readFile', function (test) {
+test('readFile', t => {
   fs.readFile(path.resolve('package.json'), 'utf8', (err, data) => {
-    test.notOk(err, 'readFile utf8 encoding should be OK')
+    t.notOk(err, 'readFile utf8 encoding should be OK')
     try {
-      test.equal(typeof data, 'string', 'readFile utf8 encoding should load package.json as string')
+      t.equal(typeof data, 'string', 'readFile utf8 encoding should load package.json as string')
       JSON.parse(data)
-      test.end()
+      t.end()
     } catch (err) {
-      test.fail('readFile utf8 encoding should load package.json')
+      t.fail('readFile utf8 encoding should load package.json')
     }
   })
 })
 
 // readFileSync
 
-tap.test('readFileSync', function (test) {
+test('readFileSync', t => {
   try {
     const data = fs.readFileSync(path.resolve('package.json'))
-    test.equal(Buffer.isBuffer(data), true, 'readFileSync should load package.json as Buffer')
+    t.equal(Buffer.isBuffer(data), true, 'readFileSync should load package.json as Buffer')
     JSON.parse(data)
-    test.end()
+    t.end()
   } catch (err) {
-    test.fail('readFileSync should load package.json')
+    t.fail('readFileSync should load package.json')
   }
 })
 
-tap.test('readFileSync', function (test) {
+test('readFileSync', t => {
   try {
     const data = fs.readFileSync(path.resolve('package.json'), 'utf8')
-    test.equal(typeof data, 'string', 'readFileSync utf8 encoding should load package.json as string')
+    t.equal(typeof data, 'string', 'readFileSync utf8 encoding should load package.json as string')
     JSON.parse(data)
-    test.end()
+    t.end()
   } catch (err) {
-    test.fail('readFileSync utf8 encoding should load package.json')
+    t.fail('readFileSync utf8 encoding should load package.json')
   }
 })
 
 // readdir
 
-tap.test('readdir', function (test) {
+test('readdir', t => {
   fs.readdir(path.resolve('test/fixtures/fs/readdir'), (err, files) => {
-    test.notOk(err, 'readdir should return no error')
-    test.equal(files.sort().join(','), 'bar,foo,tmp', 'readdir should read dir entries')
-    test.end()
+    t.notOk(err, 'readdir should return no error')
+    t.equal(files.sort().join(','), 'bar,foo,tmp', 'readdir should read dir entries')
+    t.end()
   })
 })
 
 // readdirSync
 
-tap.test('readdirSync', function (test) {
+test('readdirSync', t => {
   try {
     const files = fs.readdirSync(path.resolve('test/fixtures/fs/readdir'))
-    test.equal(files.sort().join(','), 'bar,foo,tmp', 'readdirSync should read dir entries')
-    test.end()
+    t.equal(files.sort().join(','), 'bar,foo,tmp', 'readdirSync should read dir entries')
+    t.end()
   } catch (err) {
-    test.fail(err, 'readdirSync should return no error')
+    t.fail(err, 'readdirSync should return no error')
   }
 })
 
 // chmod
 
-function testChmod (test, filename, mode) {
+function testChmod (t, filename, mode) {
   const verify = (expectMode, cb) => {
     fs.stat(filename, (err, stat) => {
-      test.notOk(err, `stat ${filename} failed`)
+      t.notOk(err, `stat ${filename} failed`)
       const newMode = stat.mode & 0o777
-      test.equal(newMode, expectMode, `stat not agree with chmod on mode: ${newMode} vs ${expectMode}`)
+      t.equal(newMode, expectMode, `stat not agree with chmod on mode: ${newMode} vs ${expectMode}`)
       cb()
     })
   }
 
   fs.stat(filename, (err, stat) => {
-    test.notOk(err, `stat ${filename} failed`)
+    t.notOk(err, `stat ${filename} failed`)
     const origMode = stat.mode & 0o777
     fs.chmod(filename, mode, (err) => {
-      test.notOk(err, `a. chmod ${mode} failed ${err}`)
+      t.notOk(err, `a. chmod ${mode} failed ${err}`)
       verify(mode, () => {
         fs.chmod(filename, origMode, (err) => {
-          test.notOk(err, `b. chmod ${mode} failed ${err}`)
+          t.notOk(err, `b. chmod ${mode} failed ${err}`)
           verify(origMode, () => {
-            test.end()
+            t.end()
           })
         })
       })
@@ -320,8 +320,8 @@ function testChmod (test, filename, mode) {
   })
 }
 
-tap.test('chmod', function (test) {
-  testChmod(test, path.resolve('test/fixtures/fs'), 0o777)
+test('chmod', t => {
+  testChmod(t, path.resolve('test/fixtures/fs'), 0o777)
 })
 
 // open
